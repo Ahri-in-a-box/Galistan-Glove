e04497799c46a7575d5f385cce2c792a9649953a
ElVaisaire <eleonore@vaissaire.fr>
2022-10-10 11:51:01 +0200

    Apple in bucket fixed
    Apples now teleport with the buket when player is moving around

--- Assets/Scripts/BucketBehavior.cs
+++ Assets/Scripts/BucketBehavior.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BucketBehavior : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
-    private float mass = 0.0f;
+    List<GameObject> objects = new List<GameObject>();
+    private decimal mass = 0.0M;
+    public bool isChanged = false;
     
     // Start is called before the first frame update
     void Start()
@@ -19,21 +22,56 @@ public class BucketBehavior : MonoBehaviour
     void Update()
     {
         text.SetText(GetMass().ToString() + " Kg");
+        if(isChanged)
+        {
+            isChanged = false;
+            Invoke("OnSelectExited", 0.5f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag != "Ground" && other.gameObject.tag != "Environment")
-            mass += other.GetComponent<Rigidbody>().mass;
+        if (other.gameObject.tag != "Ground" && other.gameObject.tag != "Environment")
+        {
+            mass += (decimal)other.GetComponent<Rigidbody>().mass;
+            other.transform.SetParent(transform);
+            objects.Add(other.gameObject);
+        }    
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag != "Ground" && other.gameObject.tag != "Environment")
-            mass -= other.GetComponent<Rigidbody>().mass;
+        {
+            mass -= (decimal)other.GetComponent<Rigidbody>().mass;
+            other.transform.SetParent(null);
+            objects.Remove(other.gameObject);
+        }
     }
 
-    public float GetMass()
+    public void OnSelectEntered()
+    {
+        foreach(GameObject obj in objects)
+        {
+            obj.GetComponent<Rigidbody>().isKinematic = true;
+            obj.GetComponent<Rigidbody>().useGravity = false;
+        }
+ 
+        isChanged = true;
+        print("Activé");
+    }
+
+    public void OnSelectExited()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.GetComponent<Rigidbody>().isKinematic = false;
+            obj.GetComponent<Rigidbody>().useGravity = true;
+        }
+        print("Désactivé");
+    }
+
+    public decimal GetMass()
     {
         return mass;
     }
