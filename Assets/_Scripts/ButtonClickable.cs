34a9f9facbad6bb254a249a661a38341dfb86468
Ahri in a box <tom.didier4@gmail.com>
2022-12-14 14:40:39 +0100

    Script Bouton Cliquable

--- Assets/_Scripts/ButtonClickable.cs
+++ Assets/_Scripts/ButtonClickable.cs
@@ -2,24 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum ButtonType
+{
+    NONE,
+    MENU,
+    OPTIONS,
+    SCENE_SWAP,
+    STATE,
+    QUIT
+}
+
 public class ButtonClickable : MonoBehaviour
 {
-
     [SerializeField] UserInterfaceManager ui;
     [SerializeField] GameObject panel;
-    private UnityEngine.UI.Button btn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        btn = GetComponent<UnityEngine.UI.Button>();    
-    }
+    [SerializeField]
+    private ButtonType m_Type = ButtonType.NONE;
 
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.tag == "Index")
         {
-            ui.OnMenuPressed(panel);
+            switch (m_Type)
+            {
+                case ButtonType.MENU:
+                    if(ui)
+                        ui.OnMenuPressed(panel);
+                    break;
+                case ButtonType.OPTIONS:
+                    break;
+                case ButtonType.SCENE_SWAP:
+                    break;
+                case ButtonType.STATE:
+                    break;
+                case ButtonType.QUIT:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
